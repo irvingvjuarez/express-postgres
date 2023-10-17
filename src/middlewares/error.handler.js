96723735e85258225653ae96a8892e26732ff47a
Irving Juárez <irving.juarez@thomsonreuters.com>
2023-10-17 09:40:36 -0600

    Error handling and error logging handlers added

--- src/middlewares/error.handler.js
+++ src/middlewares/error.handler.js
@@ -1,3 +1,8 @@
+exports.errorLogging = function errorLogging(err, req, res, next) {
+    console.error(err);
+    next(err);
+}
+
 exports.errorHandler =  function errorHandler(err, req, res, next) {
     if (err.isBoom) {
         const { output } = err;
