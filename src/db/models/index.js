4f052f69edbac00026d88e34688ad0e4a797e6e0
Irving Juárez <irving.juarez@thomsonreuters.com>
2023-10-23 18:38:48 -0600

    Customer integration done completely

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -1,7 +1,9 @@
+const { Customer, CustomerSchema } = require('./customers.model');
 const { User, UserSchema } = require('./users.model');
 
 function setupModels(sequelize) {
     User.init(UserSchema, User.config(sequelize));
+    Customer.init(CustomerSchema, Customer.config(sequelize));
 }
 
 module.exports = setupModels;
--- src/routers/v1/index.js
+++ src/routers/v1/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const UsersRouter = require('./users.router');
+const CustomerRouter = require('./customers.router');
 
 const v1Router = express.Router();
 v1Router.use('/users', UsersRouter);
+v1Router.use('/customers', CustomerRouter);
 
 module.exports = v1Router;
