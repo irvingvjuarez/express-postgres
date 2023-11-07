95ccd4b7085d9695bc88753c33ed86037d103842
Irving Juárez <irving.juarez@thomsonreuters.com>
2023-11-07 17:33:54 -0600

    Returning a token on login

--- src/routers/v1/auth.router.js
+++ src/routers/v1/auth.router.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const passport = require('passport');
+const signToken = require('../../utils/auth/token-sign');
+const config = require('../../config')
 
 const AuthRouter = express.Router()
 
@@ -7,7 +9,9 @@ AuthRouter.post(
     '/login',
     passport.authenticate('local', {session: false}),
     (req, res) => {
-        res.json({success: true,  data: req.user})
+        const payload = {...req.user.dataValues, sub: req.user.id}
+        const token = signToken(payload, config.tokenSecret);
+        res.json({success: true, token})
     }
 );
 
